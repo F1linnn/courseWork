e4d3a5888b85e19009bd01eacfe7505b82914fe6
Nikita Leleko <73548163+NikitaLeleko@users.noreply.github.com>
2023-11-07 17:49:35 +0300

    normal work

--- movie_recommendation/movies/views.py
+++ movie_recommendation/movies/views.py
@@ -9,27 +9,7 @@ from django.urls import reverse_lazy
 import csv
 
 def movie_list(request):
-    with open('D:\Pycharm Projects\courseWork\movie_recommendation\movies\mov.csv', mode='r', encoding='utf-8') as file:
-        # Читаем CSV-файл с помощью библиотеки csv
-        csv_reader = csv.DictReader(file)
-        ctr= 2
-        for row in csv_reader:
-            print(ctr)
-            # Создаем объект Movie и сохраняем его в базе данных
-            movie = Movie(
-                title=row['title'],
-                slug_title=row['title'].replace(" ", "_").lower(),
-                overview=row['overview'],
-                keywords=row['keywords'],
-                production_companies=row['production_companies'],
-                production_countries=row['production_countries'],
-                original_language=row['original_language'],
-                genres=row['genres'],
-                runtime=row['runtime'],
-                budget=int(row['budget']),
-                rating=float(row['rating'])
-            )
-            movie.save()
+   
 
     movies = Movie.objects.all()
     return render(request, 'movies/movie_list.html', {'movies': movies})
