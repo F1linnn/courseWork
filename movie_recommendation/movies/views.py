aac4c16bda6647f33cebc6f218cfb70b7c79bd2d
Nikita Leleko <73548163+NikitaLeleko@users.noreply.github.com>
2023-11-07 17:39:53 +0300

    changes

--- movie_recommendation/movies/views.py
+++ movie_recommendation/movies/views.py
@@ -6,10 +6,30 @@ from django.contrib.auth import login, authenticate
 from django.views.generic.edit import CreateView
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import reverse_lazy
-# import csv
+import csv
 
 def movie_list(request):
-
+    with open('D:\Pycharm Projects\courseWork\movie_recommendation\movies\mov.csv', mode='r', encoding='utf-8') as file:
+        # Читаем CSV-файл с помощью библиотеки csv
+        csv_reader = csv.DictReader(file)
+        ctr= 2
+        for row in csv_reader:
+            print(ctr)
+            # Создаем объект Movie и сохраняем его в базе данных
+            movie = Movie(
+                title=row['title'],
+                slug_title=row['title'].replace(" ", "_").lower(),
+                overview=row['overview'],
+                keywords=row['keywords'],
+                production_companies=row['production_companies'],
+                production_countries=row['production_countries'],
+                original_language=row['original_language'],
+                genres=row['genres'],
+                runtime=row['runtime'],
+                budget=int(row['budget']),
+                rating=float(row['rating'])
+            )
+            movie.save()
 
     movies = Movie.objects.all()
     return render(request, 'movies/movie_list.html', {'movies': movies})
@@ -54,6 +74,23 @@ def logout_success(request):
 
 def movie_detail(request, movie_id):
     movie = get_object_or_404(Movie, id=movie_id)
+    return render(request, 'movies/movie_detail.html', {'movie': movie})
+
+
+@login_required
+def add_to_history(request, movie_id):
+    movie = get_object_or_404(Movie, id=movie_id)
+
+    if movie.histories is None:
+        movie.histories = request.user
+    # Проверяем, что текущий пользователь не добавил фильм в историю просмотра
+    if movie.histories.filter(id=request.user.id).exists():
+        # Пользователь уже просматривал этот фильм
+        pass
+    else:
+        # Пользователь ещё не просматривал этот фильм, добавляем его в историю
+        movie.histories.add(request.user)
+
     return render(request, 'movies/movie_detail.html', {'movie': movie})
 
 def search_movies(request):
