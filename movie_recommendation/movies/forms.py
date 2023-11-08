da21b7b3a0b1005bbc49d20d2c697cbeafccaecd
Nickit-k-a <lemesh12345678@gmail.com>
2023-11-08 18:56:54 +0300

    forma 4erez o4ko

--- movie_recommendation/movies/forms.py
+++ movie_recommendation/movies/forms.py
@@ -5,8 +5,8 @@ from .models import *
 class RegistrationForm(UserCreationForm):
     username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-input'}))
     email = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-input'}))
-    password1 = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-input'}))
-    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-input'}))
+    password1 = forms.CharField(label="Password", widget=forms.PasswordInput(attrs={'class': 'form-input'}))
+    password2 = forms.CharField(label="Repeat password",widget=forms.PasswordInput(attrs={'class': 'form-input'}))
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
